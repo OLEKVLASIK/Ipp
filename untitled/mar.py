51677a40c69d3c78db58214bb2421e2814bf97bd
OLEKVLASIK <luckyvog@gmail.com>
2016-11-30 15:07:49 +0100

    prototyp

--- untitled/mar.py
+++ untitled/mar.py
@@ -1,100 +1,133 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Импортируем библиотеку pygame
 import pygame
 from pygame import *
 from player import *
 from blocks import *
 
+# Объявляем переменные
+WIN_WIDTH = 800  # Ширина создаваемого окна
+WIN_HEIGHT = 640  # Высота
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
+BACKGROUND_COLOR = "black"
 
-WIN_WIDTH =800 #ширина
-WIN_HEIGHT=650 #висота
-DISPLAY=(WIN_WIDTH,WIN_HEIGHT) #групировка
-BACKGROUND_COLOR="black" #колір тла
-PLATFORM_WIDTH=32
-PLATFORM_HEIGHT=32
-PLATFORM_COLOR="yellow"
 
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l + WIN_WIDTH / 2, -t + WIN_HEIGHT / 2
+
+    l = min(0, l)  # Не движемся дальше левой границы
+    l = max(-(camera.width - WIN_WIDTH), l)  # Не движемся дальше правой границы
+    t = max(-(camera.height - WIN_HEIGHT), t)  # Не движемся дальше нижней границы
+    t = min(0, t)  # Не движемся дальше верхней границы
+
+    return Rect(l, t, w, h)
 
 
 def main():
-    pygame.init()# ініціалізація pygame
-    screen = pygame.display.set_mode(DISPLAY) #cтворюєм вікно
-    pygame.display.set_caption("Mario")# назва вікна
-    bg=Surface((WIN_WIDTH,WIN_HEIGHT))# створеня видимої поверхні
+    pygame.init()  # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
+    pygame.display.set_caption("Super Mario Boy")  # Пишем в шапку
+    bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
+    # будем использовать как фон
+    bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
 
-    bg.fill(Color(BACKGROUND_COLOR)) #заливаєм її коліром
-    hero=Player(55,55)# створюєм героя(х,y)
-    left=right=False# стоїмо якшо нічого не робимо
-    up=False
-    entities = pygame.sprite.Group()
-    platforms = []
+    hero = Player(55, 55)  # создаем героя по (x,y) координатам
+    left = right = False  # по умолчанию - стоим
+    up = False
+
+    entities = pygame.sprite.Group()  # Все объекты
+    platforms = []  # то, во что мы будем врезаться или опираться
+
     entities.add(hero)
+
     level = [
-       "-------------------------",
-       "-                       -",
-       "-                       -",
-       "-                       -",
-       "-            --         -",
-       "-                       -",
-       "--                      -",
-       "-                       -",
-       "-                   --- -",
-       "-                       -",
-       "-                       -",
-       "-      ---              -",
-       "-                       -",
-       "-   -----------         -",
-       "-                       -",
-       "-                -      -",
-       "-                   --  -",
-       "-                       -",
-       "-                       -",
-       "-------------------------"] #рівеень
+        "----------------------------------",
+        "-                                -",
+        "-                       --       -",
+        "-                                -",
+        "-            --                  -",
+        "-                                -",
+        "--                               -",
+        "-                                -",
+        "-                   ----     --- -",
+        "-                                -",
+        "--                               -",
+        "-                                -",
+        "-                            --- -",
+        "-                                -",
+        "-                                -",
+        "-      ---                       -",
+        "-                                -",
+        "-   -------         ----         -",
+        "-                                -",
+        "-                         -      -",
+        "-                            --  -",
+        "-                                -",
+        "-                                -",
+        "----------------------------------"]
 
     timer = pygame.time.Clock()
-    x=y=0 #координати
-    for row in level: #весь рядок
-            for col in row:# кожен символ
-                if col == "-":
-                    pf = Platform(x,y)
-                    entities.add(pf)
-                    platforms.append(pf)
-                x=x+PLATFORM_WIDTH# блоки платформи ставляться на ширині блоків
-            y=y+PLATFORM_HEIGHT# то саме і з висотою
-            x=0 #на кожному рядку починаєм з нуля
+    x = y = 0  # координаты
+    for row in level:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
 
+            x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
 
+    total_level_width = len(level[0]) * PLATFORM_WIDTH  # Высчитываем фактическую ширину уровня
+    total_level_height = len(level) * PLATFORM_HEIGHT  # высоту
 
-    while 1: #основний цикл програми
+    camera = Camera(camera_configure, total_level_width, total_level_height)
+
+    while 1:  # Основной цикл программы
         timer.tick(60)
-        for i in pygame.event.get():
-            if i.type==QUIT:
-                raise SystemExit,"QUIT"
-
-            if i.type == KEYDOWN and i.key == K_UP:
+        for e in pygame.event.get():  # Обрабатываем события
+            if e.type == QUIT:
+                raise SystemExit, "QUIT"
+            if e.type == KEYDOWN and e.key == K_UP:
                 up = True
-            if i.type == KEYDOWN and i.key == K_LEFT:
+            if e.type == KEYDOWN and e.key == K_LEFT:
                 left = True
-            if i.type == KEYDOWN and i.key == K_RIGHT:
+            if e.type == KEYDOWN and e.key == K_RIGHT:
                 right = True
 
-            if i.type == KEYUP and i.key == K_UP:
+            if e.type == KEYUP and e.key == K_UP:
                 up = False
-            if i.type == KEYUP and i.key == K_RIGHT:
+            if e.type == KEYUP and e.key == K_RIGHT:
                 right = False
-            if i.type == KEYUP and i.key == K_LEFT:
+            if e.type == KEYUP and e.key == K_LEFT:
                 left = False
 
+        screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
 
-        screen.blit(bg,(0,0)) #кожну ітерацію треба все перремальовувати
-        hero.update(left, right, up,platforms)# рух
-        entities.draw(screen) #відображеня
+        camera.update(hero)  # центризируем камеру относительно персонажа
+        hero.update(left, right, up, platforms)  # передвижение
+        # entities.draw(screen) # отображение
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
 
-
-
-
-        pygame.display.update() # обновлення і вивід всіх змін на екран
-
+        pygame.display.update()  # обновление и вывод всех изменений на экран
 
 
 if __name__ == "__main__":
--- untitled/player.py
+++ untitled/player.py
@@ -2,13 +2,42 @@
 # -*- coding: utf-8 -*-
 
 from pygame import *
+import pyganim
+import os
+
 
 MOVE_SPEED=7
-WIDTH= 22
-HEIGHT=32
+WIDTH= 45
+HEIGHT=53
 COLOR="blue"
 JUMP_POWER = 10
 GRAVITY = 0.35
+ANIMATION_DELAY = 0.1#zmiana animacji
+ICON_DIR = os.path.dirname(__file__)
+
+ANIMATION_RIGHT = [('%s/postac/12.png' % ICON_DIR),
+            ('%s/postac/13.png' % ICON_DIR),
+            ('%s/postac/14.png' % ICON_DIR),
+            ('%s/postac/15.png' % ICON_DIR),
+            ('%s/postac/16.png' % ICON_DIR),
+            ('%s/postac/17.png' % ICON_DIR),
+            ('%s/postac/18.png' % ICON_DIR),
+            ('%s/postac/19.png' % ICON_DIR),
+            ('%s/postac/20.png' % ICON_DIR)]
+ANIMATION_LEFT = [('%s/postac/9.png' % ICON_DIR),
+            ('%s/postac/1.png' % ICON_DIR),
+            ('%s/postac/2.png' % ICON_DIR),
+            ('%s/postac/3.png' % ICON_DIR),
+            ('%s/postac/4.png' % ICON_DIR),
+            ('%s/postac/5.png' % ICON_DIR),
+            ('%s/postac/6.png' % ICON_DIR),
+            ('%s/postac/7.png' % ICON_DIR),
+            ('%s/postac/8.png' % ICON_DIR)]
+ANIMATION_JUMP_LEFT = [('%s/postac/22.png' % ICON_DIR, 0.1)]
+ANIMATION_JUMP_RIGHT = [('%s/postac/35.png' % ICON_DIR, 0.1)]
+ANIMATION_JUMP = [('%s/postac/31.png' % ICON_DIR, 0.1)]
+ANIMATION_STAY = [('%s/postac/21.png' % ICON_DIR, 0.1)]
+
 
 
 
@@ -24,19 +53,65 @@ class Player(sprite.Sprite):
         self.image.fill(Color(COLOR))
         self.rect=Rect(x,y,WIDTH,HEIGHT)#прямокутний обєкт
         self.onGround=False
+        self.image.set_colorkey(Color(COLOR)) #przezroczysty fon
+
+
+        boltAnim = []
+        for anim in ANIMATION_RIGHT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
+        self.boltAnimRight.play()
+        #        Анимация движения влево
+        boltAnim = []
+        for anim in ANIMATION_LEFT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
+        self.boltAnimLeft.play()#
+
+        self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
+        self.boltAnimStay.play()
+        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
+
+        self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
+        self.boltAnimJumpLeft.play()
+
+        self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
+        self.boltAnimJumpRight.play()
+
+        self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
+        self.boltAnimJump.play()
+
+
 
     def update(self,left,right,up, platforms):
         if up:
             if self.onGround:
                 self.yvel=-JUMP_POWER
+            self.image.fill(Color(COLOR))
+            self.boltAnimJump.blit(self.image, (0, 0))
+
         if left:
             self.xvel=-MOVE_SPEED #ліво =x-n
+            self.image.fill(Color(COLOR))
+            if up:  # для прыжка влево есть отдельная анимация
+                self.boltAnimJumpLeft.blit(self.image, (0, 0))
+            else:
+                self.boltAnimLeft.blit(self.image, (0, 0))
 
         if right:
             self.xvel=MOVE_SPEED#право = x+n
+            self.image.fill(Color(COLOR))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0, 0))
+            else:
+                self.boltAnimRight.blit(self.image, (0, 0))
 
         if not(left or right):#cтоїмо коли нема наказу йти
             self.xvel=0
+            if not up:
+               self.image.fill(Color(COLOR))
+               self.boltAnimStay.blit(self.image, (0, 0))
+
 
         if not self.onGround:
             self.yvel+=GRAVITY
